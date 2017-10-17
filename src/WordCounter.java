5936ed4d9f0b0a21b51d552f30785df085d0b7f6
ethanwc <ethanwcheatham@gmail.com>
2017-10-17 15:52:10 -0700

    test

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -1,50 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.border.Border;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Created by Ethan on 10/16/2017.
  */
-//scrollbar isn't appearing
-
 public class WordCounter {
     private static int x,y, count;
-    JScrollPane scroll = new JScrollPane();
     JTextField wordCount = new JTextField();
     JFrame window = new JFrame();
-    JPanel panel = new JPanel();
     JTextArea textField = new JTextArea(4,4);
+    JScrollPane scroll = new JScrollPane(textField);
     Font f = new Font("serif", Font.PLAIN, 50);
 
     public WordCounter() {
+        //find screen resolution for scaling
         x = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
         y = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 
+        //sets preferences
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         textField.setPreferredSize(new Dimension(x/3,y/3));
         textField.setLineWrap(true);
-
         wordCount.setSize(new Dimension(x/6,y/2));
         wordCount.setBackground(Color.RED);
         wordCount.setText("Input contains 0 words");
         wordCount.setFont(f);
         textField.setFont(f);
 
-
-        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-        window.add(panel);
+        //builds gui
         window.add(wordCount, BorderLayout.NORTH);
         window.add(textField, BorderLayout.SOUTH);
-        textField.add(scroll);
+        window.add(scroll, BorderLayout.EAST);
 
-
+        //finishes window
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
         window.pack();
 
+        //detect when a key is pressed to find entered words
         textField.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -63,6 +58,10 @@ public class WordCounter {
         });
     }
 
+    public static void main(String[] args) {
+        WordCounter wc = new WordCounter();
+    }
+
     public void updateCount(String input) {
         count = 0;
         if (input.equals("")) {
@@ -75,9 +74,5 @@ public class WordCounter {
             }
         count++;
         wordCount.setText("Input contains: " +count + " words.");
-    }
-
-    public static void main(String[] args) {
-        WordCounter wc = new WordCounter();
     }
 }
