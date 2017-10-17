2a089825272760022609efdd563c53bfe617b8c5
ethanwc <ethanwcheatham@gmail.com>
2017-10-17 15:35:23 -0700

    test

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -2,41 +2,43 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
 /**
  * Created by Ethan on 10/16/2017.
  */
+//scrollbar isn't appearing
 
-//goal is to have it count live, without hitting the count button :)
-    // use an action listiner on the text field
 public class WordCounter {
     private static int x,y, count;
+    JScrollPane scroll = new JScrollPane();
     JTextField wordCount = new JTextField();
+    JFrame window = new JFrame();
+    JPanel panel = new JPanel();
+    JTextArea textField = new JTextArea(4,4);
+    Font f = new Font("serif", Font.PLAIN, 50);
 
     public WordCounter() {
         x = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
         y = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 
-        JFrame window = new JFrame();
-        JPanel panel = new JPanel();
-        JTextArea textField = new JTextArea();
-
         textField.setPreferredSize(new Dimension(x/3,y/3));
+        textField.setLineWrap(true);
+
         wordCount.setSize(new Dimension(x/6,y/2));
-        panel.setBackground(Color.GREEN);
         wordCount.setBackground(Color.RED);
         wordCount.setText("Input contains 0 words");
-        Font f = new Font("serif", Font.PLAIN, 50);
         wordCount.setFont(f);
         textField.setFont(f);
 
-        Border border = BorderFactory.createLineBorder(Color.BLACK);
-        textField.setBorder(BorderFactory.createCompoundBorder(border,
-                BorderFactory.createEmptyBorder(200,200,200,200)));
+
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
         window.add(panel);
         window.add(wordCount, BorderLayout.NORTH);
         window.add(textField, BorderLayout.SOUTH);
-
+        textField.add(scroll);
 
 
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -46,7 +48,7 @@ public class WordCounter {
         textField.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
-                    updateCount(textField.getText());
+                updateCount(textField.getText());
             }
 
             @Override
@@ -68,9 +70,9 @@ public class WordCounter {
             return;
         }
         else
-        for (int i = 0; i < input.length(); i++) {
-            if (input.substring(i,i+1).equals(" ")) count++;
-        }
+            for (int i = 0; i < input.length(); i++) {
+                if (input.substring(i,i+1).equals(" ")) count++;
+            }
         count++;
         wordCount.setText("Input contains: " +count + " words.");
     }
